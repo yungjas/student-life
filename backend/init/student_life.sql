eba707f6a2fa66bb47989e1f60e06ecd0c8768fa
Jasmine Lim <jasmine-lim88@hotmail.com>
2022-10-05 13:43:46 +0800

    setup db for student_school

---
+++ backend/init/student_life.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE IF NOT EXISTS `student_school`;
+USE `student_school`;
+
+DROP TABLE IF EXISTS `school`;
+CREATE TABLE `school` (
+    `school_id` int(11) NOT NULL AUTO_INCREMENT,
+    `school_name` varchar(64) NOT NULL,
+    PRIMARY KEY(`school_id`)
+);
+
+DROP TABLE IF EXISTS `student`;
+CREATE TABLE `student` (
+    `student_id` int(11) NOT NULL AUTO_INCREMENT,
+    `student_name` varchar(64) NOT NULL,
+    `school_id` int NOT NULL,
+    PRIMARY KEY(`student_id`),
+    FOREIGN KEY(`school_id`) REFERENCES `school` (`school_id`)
+);
+
+INSERT INTO `school` (`school_name`) VALUES
+("School of Computing and Information Systems"),
+("School of Social Sciences"),
+("School of Business"),
+("School of Economics"),
+("School of Law");
+
+INSERT INTO `student` (`student_name`, `school_id`) VALUES
+("Jasmine", 1);
