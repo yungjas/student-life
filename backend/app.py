f63bd6f5d4f0937415e168a6dee8d4172666f2fd
Jasmine Lim <jasmine-lim88@hotmail.com>
2022-12-02 22:08:51 +0800

    comments

--- backend/app.py
+++ backend/app.py
@@ -9,6 +9,7 @@ app.register_blueprint(auth.auth_blueprint)
 
 
 if __name__ == "__main__":
-    # should be binding to 0.0.0.0 if you want the container to be accessible from outside
+    # environ variables will be either getting from docker environment is running on docker or from the .env file if running locally
     print(os.environ.get('SQLALCHEMY_DATABASE_URI'))
+    # should be binding to 0.0.0.0 if you want the container to be accessible from outside
     app.run(host="0.0.0.0", port=6010, debug=True)
